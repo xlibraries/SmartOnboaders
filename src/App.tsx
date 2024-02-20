c0173f9e8a8f82bde81bc0a461fb48ffdd0ba758
Xlib <aman9893089064@gmail.com>
2024-02-21 01:03:05 +0530

    Created UI skeleton for the review page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AuthPage from './pages/Auth/AuthPage';
 import Upload from './pages/Home/Upload';
+import PersonalDetailsForm from './pages/Review/PersonalDetails';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -9,7 +10,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<AuthPage />} />
                 <Route path="/home" element={<Upload />} />
-
+                <Route path="/review" element={<PersonalDetailsForm />} />
             </Routes>
         </Router>
     );
