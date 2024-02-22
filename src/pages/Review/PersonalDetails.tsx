1e19d2c13d05e2597907c76a300f8ec1eb88ba07
Xlib <aman9893089064@gmail.com>
2024-02-23 02:19:32 +0530

    Updated UI

--- src/pages/Review/PersonalDetails.tsx
+++ src/pages/Review/PersonalDetails.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Input, Select, DatePicker, Row, Col, Collapse } from 'antd';
+import './PersonalDetails.css'; // Import custom styles
 
 const { Option } = Select;
 const { Panel } = Collapse;
@@ -15,15 +16,15 @@ const PersonalDetails: React.FC = () => {
   }, []);
 
   return (
-    <Form layout="vertical">
+    <Form layout="vertical" className="personal-details-form">
       <Collapse defaultActiveKey={['1']}>
         {jsonData && Object.entries(jsonData).map(([section, details]) => (
-            <Panel header={section} key={section}>
+            <Panel header={section} key={section} className="personal-details-section">
                 {details.map((detail: any, index: number) => (
                     <Row gutter={16} key={index}>
                         {Object.entries(detail).map(([key, value]) => (
                             <Col span={8} key={key}>
-                                <Form.Item name={key} label={key}>
+                                <Form.Item name={key} label={key} className="personal-details-item">
                                     <Input placeholder={String(value)} />
                                 </Form.Item>
                             </Col>
